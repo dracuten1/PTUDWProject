0de93d563d8a4058fef1d5a30bb4ed88a1b844c4
❀✿Tuấn PenSpinner✿❀ <tuanpenspinner@gmail.com>
2019-05-04 20:01:16 +0700

    Thêm bài viết

---
+++ Styles/js.js
@@ -0,0 +1,21 @@
+// Đăng nhập
+$("#login").click(function () {
+  window.open('../Writer/Login.html', '_self');
+});
+//Lên đầu trang
+$(document).ready(function () {
+  $('body').append('<div id="toTop" class="btn btn-info"><span class="glyphicon glyphicon-chevron-up"><i class="fa fa-hand-o-up fa-lg"aria-hidden="true">Đầu trang</i>');
+  $(window).scroll(function () {
+    if ($(this).scrollTop() != 0) {
+      $('#toTop').fadeIn();
+    } else {
+      $('#toTop').fadeOut();
+    }
+  });
+  $('#toTop').click(function () {
+    $("html, body").animate({
+      scrollTop: 0
+    }, 600);
+    return false;
+  });
+});
