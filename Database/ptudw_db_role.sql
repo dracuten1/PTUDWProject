571a57f8e8b006cdcd1fab3e4b05ea3e3de56a60
dracuten1 <37869555+dracuten1@users.noreply.github.com>
2019-06-03 15:05:52 +0700

    Edit db

--- Database/ptudw_db_role.sql
+++ Database/ptudw_db_role.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `ptudw_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `ptudw_db`;
 -- MySQL dump 10.13  Distrib 8.0.14, for Win64 (x86_64)
 --
 -- Host: localhost    Database: ptudw_db
@@ -48,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-06-03 14:54:07
+-- Dump completed on 2019-06-03 15:05:22
