471f6cfe19096cb585bd728e65feab16c3aa0fba
Nguyễn Thanh Tuấn <tuanpenspinner@gmail.com>
2019-06-18 19:50:37 +0700

    Pagination page cho list logs
    Thêm phân trang

--- modules/blog_module.js
+++ modules/blog_module.js
@@ -11,17 +11,24 @@ module.exports = {
     select_detail_blog:(id_blog)=>{
         return blog.load(` select  blogs.writer_id,blogs.tag,blogs.title,blogs.summary,blogs.content, users.fullname,category_child.title as category_child,category_child.name as name,category.title as category,category.name as pname from users,blogs,category category_child, category category where blogs.id_blog = ${id_blog} and blogs.writer_id=users.id    and blogs.category_id=category_child.id and category_child.parent_id=category.id`)
     },
-    select_category_child: (category_child) => {
+    select_category_child: (category_child,limit,offset) => {
 
-        return blog.load(`SELECT blogs.*,categoryChild.title as category_child,categoryChild.name as Name,categoryParent.name as pName,categoryParent.title as category FROM ptudw_db.blogs blogs, ptudw_db.category categoryChild, ptudw_db.category categoryParent WHERE categoryChild.title='${category_child}' AND blogs.category_id=categoryChild.id AND categoryParent.id=categoryChild.parent_id`);
+        return blog.load(`SELECT blogs.*,categoryChild.title as category_child,categoryChild.name as Name,categoryParent.name as pName,categoryParent.title as category FROM ptudw_db.blogs blogs, ptudw_db.category categoryChild, ptudw_db.category categoryParent WHERE categoryChild.title='${category_child}' AND blogs.category_id=categoryChild.id AND categoryParent.id=categoryChild.parent_id limit  ${limit} offset  ${offset}`);
     },
+    select_category_child_count: (category_child) => {
+
+        return blog.load(`SELECT count(*) as total FROM ptudw_db.blogs blogs, ptudw_db.category categoryChild, ptudw_db.category categoryParent WHERE categoryChild.title='${category_child}' AND blogs.category_id=categoryChild.id AND categoryParent.id=categoryChild.parent_id`);
+    },
+
     select_blogs_relate: (category_child,id_blog) => {
 
         return blog.load(`SELECT blogs.*,categoryChild.title as category_child,categoryChild.name as Name,categoryParent.name as pName,categoryParent.title as category FROM ptudw_db.blogs blogs, ptudw_db.category categoryChild, ptudw_db.category categoryParent WHERE categoryChild.title='${category_child}' AND blogs.category_id=categoryChild.id AND categoryParent.id=categoryChild.parent_id AND blogs.id_blog <> ${id_blog}`);
     },
-    select_category: (category) => {
-
-        return blog.load(`SELECT blogs.*,categoryChild.title as category_child,categoryChild.name as Name,categoryParent.name as pName,categoryParent.title as category FROM ptudw_db.blogs blogs, ptudw_db.category categoryChild, ptudw_db.category categoryParent WHERE categoryParent.title='${category}' AND categoryParent.id=categoryChild.parent_id AND blogs.category_id=categoryChild.id`);
+    select_category: (category,limit,offset) => {
+        return blog.load(`SELECT blogs.*,categoryChild.title as category_child,categoryChild.name as Name,categoryParent.name as pName,categoryParent.title as category FROM ptudw_db.blogs blogs, ptudw_db.category categoryChild, ptudw_db.category categoryParent WHERE categoryParent.title='${category}' AND categoryParent.id=categoryChild.parent_id AND blogs.category_id=categoryChild.id limit  ${limit} offset  ${offset}`);
+    },
+    select_category_count: (category) => {
+        return blog.load(`SELECT count(*) as total FROM ptudw_db.blogs blogs, ptudw_db.category categoryChild, ptudw_db.category categoryParent WHERE categoryParent.title='${category}' AND categoryParent.id=categoryChild.parent_id AND blogs.category_id=categoryChild.id`);
     },
     select_part: (id_blog, id_part) => {
         return blog.load('select * from blogs where id_blog = ' + id_blog);
